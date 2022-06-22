3ec8a0c19f2daa707be7fa308c10d185636699aa
JUHUNTAE <107525806+JUHUNTAE@users.noreply.github.com>
2022-06-22 17:39:55 +0900

    Add files via upload

---
+++ While/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace first_study_project_while
+{
+    partial class btndowhileResult
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblwhileResult = new System.Windows.Forms.Label();
+            this.btnwhileResult = new System.Windows.Forms.Button();
+            this.lboxwhileResult = new System.Windows.Forms.ListBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // lblwhileResult
+            // 
+            this.lblwhileResult.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblwhileResult.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.lblwhileResult.Location = new System.Drawing.Point(12, 9);
+            this.lblwhileResult.Name = "lblwhileResult";
+            this.lblwhileResult.Size = new System.Drawing.Size(258, 29);
+            this.lblwhileResult.TabIndex = 0;
+            this.lblwhileResult.Text = "0, 0, 0, 0, 0, 0,";
+            this.lblwhileResult.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnwhileResult
+            // 
+            this.btnwhileResult.Location = new System.Drawing.Point(276, 12);
+            this.btnwhileResult.Name = "btnwhileResult";
+            this.btnwhileResult.Size = new System.Drawing.Size(106, 26);
+            this.btnwhileResult.TabIndex = 1;
+            this.btnwhileResult.Text = "로또 번호";
+            this.btnwhileResult.UseVisualStyleBackColor = true;
+            this.btnwhileResult.Click += new System.EventHandler(this.btnwhileResult_Click);
+            // 
+            // lboxwhileResult
+            // 
+            this.lboxwhileResult.FormattingEnabled = true;
+            this.lboxwhileResult.ItemHeight = 12;
+            this.lboxwhileResult.Location = new System.Drawing.Point(12, 41);
+            this.lboxwhileResult.Name = "lboxwhileResult";
+            this.lboxwhileResult.Size = new System.Drawing.Size(258, 256);
+            this.lboxwhileResult.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.panel1.Location = new System.Drawing.Point(12, 303);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(368, 10);
+            this.panel1.TabIndex = 3;
+            // 
+            // btndowhileResult
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(394, 322);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.lboxwhileResult);
+            this.Controls.Add(this.btnwhileResult);
+            this.Controls.Add(this.lblwhileResult);
+            this.Name = "btndowhileResult";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblwhileResult;
+        private System.Windows.Forms.Button btnwhileResult;
+        private System.Windows.Forms.ListBox lboxwhileResult;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ While/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace first_study_project_while
+{
+    public partial class btndowhileResult : Form
+    {
+        public btndowhileResult()
+        {
+            InitializeComponent();
+        }
+
+        private void btnwhileResult_Click(object sender, EventArgs e)
+        {
+            //1~45 6개의 번호
+            //List<int> iList = new List<int>();
+
+            int[] iArray = new int[6];
+            int iCount = 0;
+
+            StringBuilder sb = new StringBuilder();
+            Random rd = new Random();
+
+            // iArray가 다 안차면 계속 진행
+            while (Array.IndexOf(iArray , 0) != -1)
+            {
+                int iNumber = rd.Next(1, 46); //1<= x < 46
+
+                if (Array.IndexOf(iArray, iNumber) == -1)
+                {
+                    iArray[iCount] = iNumber;
+                    //sb.Append(string.Format("{0}.", iNumber)); >> 숫자크기 순서상관없이
+                    iCount++;
+                }
+
+            }
+            //작은 숫자 순서대로
+            Array.Sort(iArray);
+            sb.Append(String.Format("{0},{1},{2},{3},{4},{5}", iArray[0], iArray[1], iArray[2], iArray[3], iArray[4], iArray[5]));
+            lblwhileResult.Text = sb.ToString();
+            lboxwhileResult.Items.Add(sb.ToString());
+        }
+
+
+    }
+}
