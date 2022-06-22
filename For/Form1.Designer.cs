f1ccc0775c6b84991a6dca5df9c9e26116e1c3f8
JUHUNTAE <107525806+JUHUNTAE@users.noreply.github.com>
2022-06-22 17:39:24 +0900

    Add files via upload

---
+++ For/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace first_study_project_for_foreach
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tboxResult = new System.Windows.Forms.TextBox();
+            this.btnfor = new System.Windows.Forms.Button();
+            this.btnforeach = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tboxResult
+            // 
+            this.tboxResult.Location = new System.Drawing.Point(12, 12);
+            this.tboxResult.Multiline = true;
+            this.tboxResult.Name = "tboxResult";
+            this.tboxResult.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tboxResult.Size = new System.Drawing.Size(261, 275);
+            this.tboxResult.TabIndex = 0;
+            // 
+            // btnfor
+            // 
+            this.btnfor.Location = new System.Drawing.Point(279, 12);
+            this.btnfor.Name = "btnfor";
+            this.btnfor.Size = new System.Drawing.Size(170, 65);
+            this.btnfor.TabIndex = 1;
+            this.btnfor.Text = "for 문";
+            this.btnfor.UseVisualStyleBackColor = true;
+            this.btnfor.Click += new System.EventHandler(this.btnfor_Click);
+            // 
+            // btnforeach
+            // 
+            this.btnforeach.Location = new System.Drawing.Point(279, 83);
+            this.btnforeach.Name = "btnforeach";
+            this.btnforeach.Size = new System.Drawing.Size(170, 65);
+            this.btnforeach.TabIndex = 1;
+            this.btnforeach.Text = "foreach 문";
+            this.btnforeach.UseVisualStyleBackColor = true;
+            this.btnforeach.Click += new System.EventHandler(this.btnforeach_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(461, 299);
+            this.Controls.Add(this.btnforeach);
+            this.Controls.Add(this.btnfor);
+            this.Controls.Add(this.tboxResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tboxResult;
+        private System.Windows.Forms.Button btnfor;
+        private System.Windows.Forms.Button btnforeach;
+    }
+}
+
---
+++ For/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace first_study_project_for_foreach
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnfor_Click(object sender, EventArgs e)
+        {
+            tboxResult.Text = String.Empty; 
+
+            StringBuilder sb = new StringBuilder();
+
+            //int iResult = 0;
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    iResult = iResult + i;
+
+            //    sb.Append(string.Format("1에서 {0}까지 더하면 {1}\r\n", i, iResult));
+            // }
+
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int a = 1; a <= 3; a++)
+                {
+                    sb.Append(string.Format("{0}회차  {1} 스테이지 진행 중 . . . \r\n", i, a));
+                }
+            }
+
+                tboxResult.Text = sb.ToString();
+        }
+
+        private void btnforeach_Click(object sender, EventArgs e)
+        {
+            tboxResult.Text = string.Empty;
+
+            StringBuilder sb = new StringBuilder();
+
+            string[] strArray = { "나연", "정연", "모모", "사나", "지효", "미나", "다현", "쯔위", "채영" };
+
+            int i = 1;
+
+            foreach (var item in strArray)
+            {
+                sb.Append(string.Format("{0} 선생님은 {1}반 입니다.  \r\n", item, i++));
+            }
+
+            tboxResult.Text = sb.ToString();
+        }
+    }
+}
